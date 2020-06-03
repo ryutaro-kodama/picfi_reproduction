9b23a13e59b9706302ce1a1e25f2fbc31652df13
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-06-03 12:54:50 +0900

    手動挿入ラベルが取得できるか確認のためのアセンブリファイル作成

---
+++ specimen/function_address_violation.s
@@ -0,0 +1,83 @@
+	.file	"function_address_violation.c"
+	.section	.rodata
+.LC0:
+	.string	"foo called"
+	.text
+	.globl	foo
+	.type	foo, @function
+foo:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	foo, .-foo
+	.section	.rodata
+.LC1:
+	.string	"bar called"
+	.text
+	.globl	bar
+	.type	bar, @function
+bar:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC1, %edi
+	call	puts
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	bar, .-bar
+	.globl	fp
+	.data
+	.align 8
+	.type	fp, @object
+	.size	fp, 8
+fp:
+	.quad	foo
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	$bar, -8(%rbp)
+__picfi_foo:
+	movq	fp(%rip), %rdx
+	movl	$0, %eax
+	call	*%rdx
+	movq	-8(%rbp), %rdx
+	movl	$0, %eax
+	call	*%rdx
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.5) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
